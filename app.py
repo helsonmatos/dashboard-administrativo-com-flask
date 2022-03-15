8cd212f4f1d8fdc98debbfea61c27397f4f2ac6f
Helson Matos <helson.11@hotmail.com>
2022-03-15 17:05:03 -0300

    model parte 5

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from config import app_config, app_active
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    
+    app.secret_key = config.SECRET
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    config.APP = app
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+
+
+    @app.route('/')
+    def index():
+        return 'oi'
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
-from pickle import TRUE
+from app import create_app
+from config import app_config, app_active
 
+config = app_config[app_active]
+config.APP = create_app(app_active)
 
-class Config():
-    CSRF_ENABLE = True
+if __name__ == '__main__':
+    config.APP.run(host=config.IP_HOST, port=config.PORT_HOST)
